a725adbd44bac6f4e02e0d3024fa048b84ec0c9b
郭理勇 <guoliyong@guoliyongdeMacBook-Air.local>
2018-08-30 21:02:45 +0800

    convolution in time domain is the same as multiplication in frequency domain, inspired by https://dsp.stackexchange.com/questions/42712/convolution-in-time-domain-is-not-same-as-multiplication-in-frequency-domain-in

--- convolve_time_multiply_frequency.py
+++ convolve_time_multiply_frequency.py
@@ -2,3 +2,18 @@ import numpy as np
 
 a=np.array([1,2,3,4,5])
 b=np.array([6,7,8,9,10])
+print("should set fft lenght or will wrong")
+x=np.fft.fft(a)
+y=np.fft.fft(b)
+print np.abs(np.fft.ifft(x*y))
+print np.convolve(a,b)
+print("set fft lenght as n1+n2-1")
+x1=np.fft.fft(a,9)
+y1=np.fft.fft(b,9)
+print("inverse of multiplication of frequency domain")
+print np.abs(np.fft.ifft(x1*y1))
+print np.convolve(a,b)
+
+print("fft of convolution of time domain")
+print np.fft.fft(np.convolve(a,b),9)
+print x1*y1
